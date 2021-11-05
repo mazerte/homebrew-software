a53adbebce4899bdd3f4a22d1297f5f0f77b5885
Mathieu Desvé <236866+mazerte@users.noreply.github.com>
2021-11-05 21:28:32 +0100

    Add DxO PureRAW

---
+++ Casks/dxo-pureraw.rb
@@ -0,0 +1,11 @@
+cask "dxo-pureraw" do
+  desc "Enhance your RAW files to open up even more possibilities with Adobe Photoshop and Lightroom®"
+  homepage "https://www.dxo.com/dxo-pureraw/"
+  url "https://download-center.dxo.com/PureRaw/v1/Mac/DxO_PureRaw_v1.dmg"
+  sha256 :no_check
+  
+  version "1.0"
+  name "DxO PureRAW"
+
+  app "DxO PureRAW.app"
+end
