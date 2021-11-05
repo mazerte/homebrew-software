2db9c9bb469abe0a02c2e20a45e275ca5d7fe80f
Mathieu Desvé <236866+mazerte@users.noreply.github.com>
2021-11-05 20:17:07 +0100

    Fix implementation

---
+++ Casks/optyx.rb
@@ -0,0 +1,11 @@
+cask "optyx" do
+  desc "Cull 1000 photos in 60 seconds."
+  homepage "https://www.optyx.app/"
+  url "https://cdn.optyx.app/optyx2/mac/x64/Optyx-latest.dmg"
+  sha256 :no_check
+  
+  version "2.0"
+  name "Optyx 2"
+
+  app "Optyx 2.app"
+end
