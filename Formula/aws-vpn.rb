e0d2a4e69e788ce7adfb916add22f0f6360bfda2
Mathieu Desvé <mathieu.desve@gmail.com>
2021-12-06 12:14:48 +0100

    aws-vpn 0.1.10

--- Formula/aws-vpn.rb
+++ Formula/aws-vpn.rb
@@ -1,8 +1,8 @@
 class AwsVpn < Formula
   desc "Easy CLI to create and manage vpn connection to your AWS VPC"
   homepage "https://github.com/mazerte/aws-vpn"
-  url "https://github.com/mazerte/aws-vpn/releases/download/v0.1.9/aws-vpn.tar.gz"
-  sha256 "71e050377fb875d94a6f3fa696e25e5a04422885868cb8a71578645ab8813b3c"
+  url "https://github.com/mazerte/aws-vpn/releases/download/v0.1.10/aws-vpn.tar.gz"
+  sha256 "377d6b6ec5f07a7aba9d938463e1b5032919bf879e77fbf506d3e30037c2bec8"
   license "MIT"
 
   depends_on "awscli"
